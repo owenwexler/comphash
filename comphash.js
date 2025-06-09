4163d2bafd8835eeffa820034fbdaa15b8d2257d
Owen Wexler <owenbwexler@gmail.com>
2025-06-08 20:23:00 -0400

    Add missing semicolons

--- comphash.js
+++ comphash.js
@@ -5,10 +5,10 @@ const args = process.argv.slice(2);
 
 const hash1 = fs.readFileSync(args[0], 'utf8');
 const hash2 = fs.readFileSync(args[1], 'utf8');
-console.log(hash1)
-console.log(hash2)
+console.log(hash1);
+console.log(hash2);
 
-console.log('COMPARING HASHES...')
+console.log('COMPARING HASHES...');
 const result = comphash(hash1, hash2);
 console.log('result: ', result);
 
